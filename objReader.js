fab6c613d06c26dd1db160333b276c51c53c1f04
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-04-04 11:17:27 -0300

    Add initial files with basic file reading

---
+++ objReader.js
@@ -0,0 +1,8 @@
+function loadObjFile(data) {
+
+    console.log(data);
+	// TO DO:   (i) Parse OBJ file and extract vertices and normal vectors
+	// TO DO:  (ii) If normal vectors are not in the file, you will need to calculate them
+	// TO DO: (iii) Return vertices and normals and any associated information you might find useful
+
+}
---
+++ objViewer.js
@@ -0,0 +1,232 @@
+
+
+var program;
+var canvas;
+var gl;
+
+var numVertices  = 36;
+
+var pointsArray = [];
+var normalsArray = [];
+
+var vertices = [
+        vec4( -0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5,  0.5,  0.5, 1.0 ),
+        vec4( 0.5,  0.5,  0.5, 1.0 ),
+        vec4( 0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5, -0.5, -0.5, 1.0 ),
+        vec4( -0.5,  0.5, -0.5, 1.0 ),
+        vec4( 0.5,  0.5, -0.5, 1.0 ),
+        vec4( 0.5, -0.5, -0.5, 1.0 )
+    ];
+
+var lightPosition = vec4( 10.0, 10.0, 10.0, 0.0 );
+var lightAmbient = vec4( 0.2, 0.2, 0.2, 1.0 );
+var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+
+var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0);
+var materialSpecular = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialShininess = 100.0;
+
+// transformation and projection matrices
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
+
+//var ctm;
+var ambientColor, diffuseColor, specularColor;
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+var axis = 1;
+var theta =[0, 0, 0];
+
+var thetaLoc;
+
+// camera definitions
+var eye = vec3(1.0, 0.0, 0.0);
+var at = vec3(0.0, 0.0, 0.0);
+var up = vec3(0.0, 1.0, 0.0);
+
+var cradius = 1.0;
+var ctheta = 0.0;
+var cphi = 0.0;
+
+// our universe
+var xleft = -1.0;
+var xright = 1.0;
+var ybottom = -1.0;
+var ytop = 1.0;
+var znear = -100.0;
+var zfar = 100.0;
+
+var flag = true;
+
+// generate a quadrilateral with triangles
+function quad(a, b, c, d) {
+
+     var t1 = subtract(vertices[b], vertices[a]);
+     var t2 = subtract(vertices[c], vertices[b]);
+     var normal = vec4(cross(t1, t2), 0);
+
+     pointsArray.push(vertices[a]); 
+     normalsArray.push(normal); 
+     pointsArray.push(vertices[b]); 
+     normalsArray.push(normal); 
+     pointsArray.push(vertices[c]); 
+     normalsArray.push(normal);   
+     pointsArray.push(vertices[a]);  
+     normalsArray.push(normal); 
+     pointsArray.push(vertices[c]); 
+     normalsArray.push(normal); 
+     pointsArray.push(vertices[d]); 
+     normalsArray.push(normal);    
+}
+
+// define faces of a cube
+function colorCube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+window.onload = function init() {
+
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    // create viewport and clear color
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.5, 0.5, 0.5, 1.0 );
+    
+    // enable depth testing for hidden surface removal
+    gl.enable(gl.DEPTH_TEST);
+
+    //  load shaders and initialize attribute buffers
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // draw simple cube for starters
+    colorCube();
+
+    // create vertex and normal buffers
+    createBuffers();
+
+    thetaLoc = gl.getUniformLocation(program, "theta"); 
+
+    // create light components
+    ambientProduct = mult(lightAmbient, materialAmbient);
+    diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    specularProduct = mult(lightSpecular, materialSpecular);
+
+    // create model view and projection matrices
+    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+    projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
+
+    document.getElementById("ButtonX").onclick = function(){axis = xAxis;};
+    document.getElementById("ButtonY").onclick = function(){axis = yAxis;};
+    document.getElementById("ButtonZ").onclick = function(){axis = zAxis;};
+    document.getElementById("ButtonT").onclick = function(){flag = !flag;};
+
+    document.getElementById('files').onchange = function (evt) {
+
+        var files = evt.target.files; // FileList object
+
+        // files is a FileList of File objects. List some properties.
+        for (var i = 0, f; f = files[i]; i++) {
+
+            console.log(escape(f.name));
+            var reader = new FileReader();
+            
+            reader.onload = (function(theFile) {
+                return function(e) {
+                    // Render thumbnail.
+                    console.log(e.target.result.split("\n"));
+                };
+            })(f);
+            
+            reader.readAsBinaryString(f);
+        }
+
+        // FileReader.readAsBinaryString()
+        // loadObject(files);
+        // TO DO: load OBJ file and display
+    };
+
+    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"),
+       flatten(ambientProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"),
+       flatten(diffuseProduct) );
+    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), 
+       flatten(specularProduct) );	
+    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), 
+       flatten(lightPosition) );
+       
+    gl.uniform1f(gl.getUniformLocation(program, 
+       "shininess"),materialShininess);
+    
+    render();
+}
+
+var render = function() {
+            
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            
+    if (flag) theta[axis] += 2.0;
+            
+    eye = vec3(cradius * Math.sin(ctheta) * Math.cos(cphi),
+               cradius * Math.sin(ctheta) * Math.sin(cphi), 
+               cradius * Math.cos(ctheta));
+
+    modelViewMatrix = lookAt(eye, at, up);
+              
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[xAxis], [1, 0, 0] ));
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[yAxis], [0, 1, 0] ));
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[zAxis], [0, 0, 1] ));
+    
+    projectionMatrix = ortho(xleft, xright, ybottom, ytop, znear, zfar);
+
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
+
+    gl.drawArrays( gl.TRIANGLES, 0, numVertices );
+            
+    requestAnimFrame(render);
+}
+
+function createBuffers(points, normals) {
+
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
+    
+    var vNormal = gl.getAttribLocation( program, "vNormal" );
+    gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vNormal );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
+    
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+}
+
+function loadObject(data) {
+
+    // TO DO: convert strings into array of vertex and normal vectors
+    var result = loadObjFile(data);
+
+    // TO DO: apply transformation to the object so that he is centered at the origin
+
+}
